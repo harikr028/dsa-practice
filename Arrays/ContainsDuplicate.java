e97e3afea1cc0492dd3c1b2fb0760cc76eaefa30
harikr028 <harikr028@gmail.com>
2026-03-02 19:57:52 +0530

    Day 05 | Arrays | Contains Duplicate
     Check if any number repeats in the array.

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,18 @@
+public class ContainsDuplicate {
+    public static boolean hasDuplicate(int numbers[]) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                if (numbers[i]==numbers[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+    public static void main(String[] args) {
+        int numbers[] = {1,2,3,4,1};
+        System.out.println(hasDuplicate(numbers));
+
+    }
+}
