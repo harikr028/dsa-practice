56e3173c79286dac5fda90ac079d4de734f5b866
harikr028 <harikr028@gmail.com>
2026-03-04 17:56:07 +0530

    Day 07 | Sorting | Selection Sort

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,25 @@
+package sorting;
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = {5,2,9,1,3};
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j]<arr[minIndex]) {
+                    minIndex = j;
+                    
+                }
+                
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+            
+        }
+
+        for (int i : arr) {
+            System.out.print(i +" ");
+        }
+    }
+}
